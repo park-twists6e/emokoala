11689e2354dbca98838e6f5e117e2b722736c88e
park-twists <park-twists6e@icloud.com>
2026-06-27 18:56:18 -0700

    Config GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://park-twists6e.github.io",
+  base: "/emokoala",
+});
